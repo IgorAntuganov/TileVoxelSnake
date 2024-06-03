37f93f5e67c8e7313833f094ae1093455c57a76c
Игорь <iantuganov@yandex.ru>
2024-06-04 03:18:45 +0500

    New: add perspective effect Fix: partly fixed incorrect pixels lines width Also: renamed 3d.py to 3d_tools.py

--- 3d_tools.py
+++ 3d_tools.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 
 
 class SidesDrawer:
@@ -17,7 +18,6 @@ class SidesDrawer:
         :param height: can be negative if top below bottom on screen
         :param offset: difference by x between the upper-left and lower-left corners
         """
-        widths_diff = top_width - bot_width
         if offset > 0:
             im_width = max(top_width, bot_width+offset)
         else:
@@ -25,15 +25,29 @@ class SidesDrawer:
         im_height = abs(height)
 
         image = pg.Surface((im_width, im_height), pg.SRCALPHA)
-
+        image.fill((255, 0, 0))
         for i in range(im_height):
             part = i/im_height
             if offset > 0:
                 im_x = int(offset * part)
+                right_top = top_width
+                right_bottom = offset + bot_width
             else:
                 im_x = int(abs(offset) * (1-part))
-            str_width = int(top_width - widths_diff * part)
-            pixel_string = self.get_pixel_string_fast(texture, str_width, part)
+                right_top = top_width + abs(offset)
+                right_bottom = bot_width
+            rights_diffs = right_top - right_bottom
+            str_width = int(right_top - rights_diffs * part) - im_x
+
+            # a0 = 1
+            # a1 = 0
+            # z0 = 1
+            z1 = bot_width/top_width
+            # part_with_perspective = (part * a0/z0 + (1-part) * a1/z1)/(part * 1/z0 + (1-part) * 1/z1)
+            part_with_perspective = part / (part + (1 - part) / z1)
+
+            pixel_string = self.get_pixel_string_fast(texture, str_width, part_with_perspective)
+
             if height > 0:
                 im_y = i
             else:
@@ -65,18 +79,37 @@ class SidesDrawer:
         pstring.blit(resized, (0, 0))
         return pstring
 
+    def get_double_pixel_string_fast(self, texture, width, part) -> pg.Surface:
+        pstring = pg.Surface((width, 1), pg.SRCALPHA)
+        texture_y = int(part * texture.get_height())
+        crop = pg.Surface((texture.get_width(), 1))
+        crop.blit(texture, (0, -texture_y))
+        #crop.set_alpha(128)
+        crop2 = pg.Surface((texture.get_width(), 1))
+        crop2.blit(texture, (0, -texture_y+1))
+        crop2.set_alpha(128)
+        crop.blit(crop2, (0, 0))
+        resized = pg.transform.scale(crop, (width, 1))
+        pstring.blit(resized, (0, 0))
+        return pstring
 
 
 def test():
     s = SidesDrawer()
 
-    grass = pg.image.load('grass.png')
+    grass_png = pg.image.load('grass.png')
+    test_width = 64
+    test_height = 64
+    grass = pg.Surface((test_width, test_height))
+    for i in range(test_width//16):
+        for j in range(test_height//16):
+            grass.blit(grass_png, (i*16, j*16))
     clock = pg.time.Clock()
     scr = pg.display.set_mode((1600, 900))
-    top = 50
-    bot = 30
-    h = 20
-    off = 20
+    top = test_width
+    bot = test_width
+    h = test_height
+    off = 0
 
     while True:
         events = pg.event.get()
@@ -84,18 +117,18 @@ def test():
             if event.type == pg.QUIT:
                 exit()
         pressed = pg.key.get_pressed()
-        if pressed[pg.K_q] and top >= 5:
+        if pressed[pg.K_q] and top > 5:
             top -= 5
         if pressed[pg.K_w]:
             top += 5
-        if pressed[pg.K_a] and bot >= 5:
+        if pressed[pg.K_a] and bot > 5:
             bot -= 5
         if pressed[pg.K_s]:
             bot += 5
         if pressed[pg.K_e]:
-            h -= 5
+            h -= 5 if abs(h) > 20 else 1
         if pressed[pg.K_d]:
-            h += 5
+            h += 5 if abs(h) > 20 else 1
         if pressed[pg.K_r]:
             off += 5
         if pressed[pg.K_f]:
@@ -103,8 +136,9 @@ def test():
 
         scr.fill((0, 0, 0))
         trap = s.get_hor_trapezoid(grass, top, bot, h, off)
-        scr.blit(trap, (100, 100))
-        clock.tick(60)
+
+        scr.blit(trap, (0, 0))
+        clock.tick(120)
         fps = clock.get_fps()
         pg.display.set_caption(str(fps))
         print(len(s.cache))
